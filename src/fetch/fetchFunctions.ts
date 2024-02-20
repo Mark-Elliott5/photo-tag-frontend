4874bd64f270821edd23432c2dbd81905c91cf0c
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2024-02-20 04:55:02 -0500

    Fetches updated to use Promise.reject(err)
    This eliminates "unknown" from the response type for axios requests,
    simplifying and correcting response handling.

--- src/fetch/fetchFunctions.ts
+++ src/fetch/fetchFunctions.ts
@@ -1,49 +1,55 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 async function getLeaderboard() {
   try {
-    const response = await axios.get(
-      window.location.hostname + '/api/leaderboard'
-    );
+    const response: AxiosResponse<{ name: string; time: string }[]> =
+      await axios.get(window.location.hostname + '/api/leaderboard');
     // return a leaderboard json
     return response;
   } catch (error) {
-    console.log(`Axios GET leaderboad error: ${error}`);
+    console.error(`Axios GET leaderboad error: ${error}`);
+    return Promise.reject(error);
   }
 }
 
 async function startGame() {
   try {
     // { withCredentials: true } needed only if not accessing the same domain
-    const response = await axios.get(window.location.hostname + '/api/start');
+    const response: AxiosResponse<{ characters: string[] }> = await axios.get(
+      window.location.hostname + '/api/start'
+    );
     return response;
   } catch (error) {
-    throw new Error(`Axios GET anon cookie failure: ${error}`);
+    console.error(`Axios GET startGame() failure: ${error}`);
+    return Promise.reject(error);
   }
 }
 
 async function submitName(name: string) {
   try {
-    const response = await axios.post(
+    const response: AxiosResponse<{ accepted: boolean }> = await axios.post(
       window.location.hostname + '/api/name',
       name
     );
     // return a leaderboard json
     return response;
   } catch (error) {
-    console.log(`Axios POST name error: ${error}`);
+    console.error(`Axios POST name error: ${error}`);
+    return Promise.reject(error);
   }
 }
 
 async function guessWaldo(coords: { x: number; y: number }, waldo: string) {
   try {
-    const response = await axios.post(window.location.hostname + '/api/waldo', {
-      coords,
-      waldo,
-    });
+    const response: AxiosResponse<{ correct: boolean; win: boolean }> =
+      await axios.post(window.location.hostname + '/api/waldo', {
+        coords,
+        waldo,
+      });
     return response;
   } catch (error) {
-    console.log(`Axios POST guess error: ${error}`);
+    console.error(`Axios POST guess error: ${error}`);
+    return Promise.reject(error);
   }
 }
 
