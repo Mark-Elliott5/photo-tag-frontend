14d16d56cdfc81c66873ff02a44b6132d488c2a6
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2024-05-26 09:31:22 -0400

    Feat: CharacterPortrait rounded corners
    It looks better.

--- src/components/CharacterPortraits.tsx
+++ src/components/CharacterPortraits.tsx
@@ -15,7 +15,7 @@ function CharacterPortraits({
             <img
               key={key}
               title={key}
-              className='sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 w-6 h-6 aspect-square hover:scale-300 hover:-translate-y-9 hover:z-10 active:scale-200'
+              className='sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 w-6 h-6 aspect-square hover:scale-300 hover:-translate-y-9 hover:z-10 rounded active:scale-200'
               src={`${key.toLowerCase()}.jpg`}
             />
           )
